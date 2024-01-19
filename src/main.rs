d758ec6c32de91d11c5ecd8563bb5733aa1c0bc7
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2024-01-19 14:05:51 +0000

    Change library and use reqwest instead of hyper

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,22 @@
 #![warn(rust_2018_idioms)]
-
-use http_body_util::{BodyExt, Empty};
-use hyper::body::Bytes;
-use hyper::Request;
-use hyper_util::rt::TokioIo;
-use tokio::io::{self, AsyncWriteExt as _};
-use tokio::net::TcpStream;
+#![deny(warnings)]
 
 // A simple type alias so as to DRY.
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Some simple CLI args requirements...
-    let url = "http://httpbin.org/ip".parse::<hyper::Uri>()?;
-
-    if url.scheme_str() != Some("http") {
-        println!("This example only works with 'http' URLs.");
-        return Ok(());
-    }
-
-    fetch_url(url).await
-}
-
-async fn fetch_url(url: hyper::Uri) -> Result<()> {
-    let host = url.host().expect("uri has no host");
-    let port = url.port_u16().unwrap_or(80);
-    let addr = format!("{}:{}", host, port);
-    let stream = TcpStream::connect(addr).await?;
-    let io = TokioIo::new(stream);
-
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            println!("Connection failed: {:?}", err);
-        }
-    });
-
-    let authority = url.authority().unwrap().clone();
-
-    let path = url.path();
-    let req = Request::builder()
-        .uri(path)
-        .header(hyper::header::HOST, authority.as_str())
-        .body(Empty::<Bytes>::new())?;
-
-    let mut res = sender.send_request(req).await?;
-
-    println!("Response: {}", res.status());
-    println!("Headers: {:#?}\n", res.headers());
-
-    // Stream the body, writing each chunk to stdout as we get it
-    // (instead of buffering and printing at the end).
-    while let Some(next) = res.frame().await {
-        let frame = next?;
-        if let Some(chunk) = frame.data_ref() {
-            io::stdout().write_all(&chunk).await?;
-        }
-    }
-
-    println!("\n\nDone!");
-
+    let timeout = std::time::Duration::new(10, 0);
+    let proxy_url = "http://127.0.0.1:3128";
+    let target_url = "https://httpbin.org/ip";
+    let proxy = reqwest::Proxy::all(proxy_url)?;
+    let client = reqwest::Client::builder()
+        .proxy(proxy)
+        .timeout(timeout)
+        .build()?;
+    let response = client.get(target_url).send().await?;
+    println!("Status: {}", response.status());
+    let body = response.text().await?;
+    println!("Body: {}", body);
     Ok(())
 }
